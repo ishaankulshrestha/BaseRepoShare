461611718367af94144fe7b4d41bddcc192672cd
ishaankulshrestha <ishaan@Lu-jDFBG32>
2017-07-10 16:31:37 +0530

    Added comments and actual implementation.

--- UsingCollectionAndGenerics/src/com/ishaank/learn/usingcollectionandgenerics/UsingComparatorDemo.java
+++ UsingCollectionAndGenerics/src/com/ishaank/learn/usingcollectionandgenerics/UsingComparatorDemo.java
@@ -1,5 +1,6 @@
 package com.ishaank.learn.usingcollectionandgenerics;
 
+// importing java.utils
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
@@ -7,17 +8,24 @@ import java.util.Scanner;
 public class UsingComparatorDemo {
 	
 	public static void main(String[] args) {
+		
+		//Setting up scanner to read values
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-
+        
+        //Making array of player class
         Player[] player = new Player[n];
+        
+        //Instantiating class implementing comparator
         Checker checker = new Checker();
         
+        //Reading data from scanner object
         for(int i = 0; i < n; i++){
             player[i] = new Player(scan.next(), scan.nextInt());
         }
         scan.close();
-     
+        
+        //Sorting on based of comparator so defined in other class
         Arrays.sort(player, checker);
         for(int i = 0; i < player.length; i++){
             System.out.printf("%s %s\n", player[i].name, player[i].score);
@@ -25,6 +33,7 @@ public class UsingComparatorDemo {
     }
 }
 	
+	//Defining class
 	class Player{
 	    String name;
 	    int score;
@@ -35,6 +44,8 @@ public class UsingComparatorDemo {
 	    }
 	}
 	
+	
+	//Defining class implementing comparator 
 	class Checker implements Comparator<Object>{
 	    
 	    @Override
@@ -44,14 +55,14 @@ public class UsingComparatorDemo {
 	        Player P1 = (Player)arg0;
 	        Player P2 = (Player)arg1;
 	        
+	        //In case if score is same then decide lexicographically
 	        if (P1.score == P2.score) 
 	        	return(P1.name.compareTo(P2.name));
 	       
 	        else if (P1.score > P2.score)
 	            return -1;
 	        else 
-	            return 1;
-	        
+	            return 1;	        
 	        
 		}  
 	        
